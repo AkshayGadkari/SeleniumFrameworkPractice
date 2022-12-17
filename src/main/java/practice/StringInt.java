4b4a34c0821ba9af95e514a93228fb777238baec
AkshayGadkari <akshaygadkari24@gmail.com>
2022-12-18 02:05:38 +0530

    Practice package added

---
+++ src/main/java/practice/StringInt.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class StringInt {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        String stri= "200";
+        int i=10;
+
+
+        //String to integer Conversion
+        try {
+            System.out.println("Integer.parseInt(str) = " + Integer.parseInt(str));
+        }
+        catch(NumberFormatException nfe)
+        {
+            System.out.println(nfe);
+            nfe.printStackTrace();
+        }
+
+        System.out.println("Integer.parseInt(stri) = " + Integer.parseInt(stri));
+
+
+        //integer to String Conversions
+        String is =Integer.toString(i);
+
+        System.out.println(is);
+
+    }
+}
